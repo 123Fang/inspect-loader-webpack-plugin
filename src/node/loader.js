19e16977488f2c0564aadba2180acd98d6402153
123Fang <38906235+123Fang@users.noreply.github.com>
2024-06-03 16:08:57 +0800

    fix: 添加注释

--- src/node/loader.js
+++ src/node/loader.js
@@ -13,11 +13,15 @@ const fse = require('fs-extra');
 function getLoaderName(loaderPath) {
   let subLoaderName = '';
   if (/vue\-loader/.test(loaderPath) && /loaders/.test(loaderPath)) {
+    // const fileName = path.basename('/path/to/file.txt');
+    // console.log(fileName); // 输出: file.txt
     subLoaderName = path.basename(loaderPath);
     subLoaderName = subLoaderName.slice(0, subLoaderName.indexOf('.'));
   }
+  // 获取path.dirname(loaderPath) 目录下的package.json文件路径
   const packageJsonPath = findUp.sync('package.json', { cwd: path.dirname(loaderPath) });
   if (packageJsonPath) {
+    // 读这个package.json文件，返回一个json对象
     const pkg = fse.readJSONSync(packageJsonPath);
     return pkg.name + (subLoaderName ? ` 中的 ${subLoaderName}` : '');
   }
@@ -28,7 +32,7 @@ const PLUGIN_NAME = 'my-inspect-webpack-plugin';
 
 
 function pitch(p) { // p绝对路径
-  // collectCode
+  // collectCode 函数，在插件中注入到loaderContext对象上，用来收集loader处理后的内容
   const collectCode = this[PLUGIN_NAME];
   const loaderPaths = this.loaders.map((loader) => loader.path);
 
@@ -36,28 +40,49 @@ function pitch(p) { // p绝对路径
   hijackLoaders(loaderPaths, (loader, path) => {
     const loaderName = getLoaderName(path);
 
-    const wrapFunc = (func) => function proxyLoader(...args1) {
-      /**
-       * 收集 source
-       * 思路：loader函数转换后的字符串，需要继续往下传给下一个loader或丢给webpack。往下传的方式是以下三种：
-       * 1 LoaderContext.callback(source)
-       * 2 const async = LoaderContext.async(); async(source)
-       * 3 return data
-       *
-       * 解决方案；
-       * 针对1,2：
-       *    拦截callback和async方法，即可拿到source。
-       * 针对3:
-       *    拿到return的结果，即可拿到source。
-       * * */
-      // 针对1,2：拦截 LoaderContext.callback 和 LoaderContext.async
-      const almostThis = Object.assign({}, this, {
-        async: function (...asyncArgs) {
-          const asyncCallback = this.async.apply(this);
-          const { resourcePath } = this;
-          return function (...args) {
+    const wrapFunc = (func) => {
+      return function proxyLoader(...args1) {
+        /**
+         * 收集 source
+         * 思路：loader函数转换后的字符串，需要继续往下传给下一个loader或丢给webpack。往下传的方式是以下三种：
+         * 1 LoaderContext.callback(source)
+         * 2 const async = LoaderContext.async(); async(source)
+         * 3 return data
+         *
+         * 解决方案；
+         * 针对1,2：
+         *    拦截callback和async方法，即可拿到source。
+         * 针对3:
+         *    拿到return的结果，即可拿到source。
+         * * */
+        // 针对1,2：拦截 LoaderContext.callback 和 LoaderContext.async
+        const almostThis = Object.assign({}, this, {
+          async: function (...asyncArgs) {
+            const asyncCallback = this.async.apply(this);
+            const { resourcePath } = this;
+            return function (...args) {
+              const end = Date.now();
+              const [, source] = args;
+              // 收集loder处理后的结果
+              collectCode({
+                id: resourcePath,
+                name: `${loaderName}【${func.type}】`,
+                result: source,
+                beforeResult: args1[0] ? args1[0] : args1[1],
+                type: func.type,
+                start,
+                end,
+              });
+              return asyncCallback.apply(this, args);
+            };
+          }.bind(this),
+
+          callback: function (...args) {
+            // const start = Date.now()
+            const { resourcePath } = this;
             const end = Date.now();
             const [, source] = args;
+            // 收集loder处理后的结果
             collectCode({
               id: resourcePath,
               name: `${loaderName}【${func.type}】`,
@@ -66,56 +91,39 @@ function pitch(p) { // p绝对路径
               type: func.type,
               start,
               end,
+              test: 'for callback',
             });
-            return asyncCallback.apply(this, args);
-          };
-        }.bind(this),
-        
-        callback: function (...args) {
+            this.callback.apply(this, args);
+          }.bind(this),
+        });
+
+
+        let start = Date.now();
+        // 针对3: 拿到return的结果，即可拿到source。
+        const ret = func.apply(almostThis, args1); // run 这个 loader。args 是代码字符串。
+        let end = Date.now();
+        if (ret && typeof ret === 'string') {
           // const start = Date.now()
-          const { resourcePath } = this;
-          const end = Date.now();
-          const [, source] = args;
+          let { resourcePath } = this;
+          // const end = Date.now();
+          // 收集loder处理后的结果
           collectCode({
             id: resourcePath,
             name: `${loaderName}【${func.type}】`,
-            result: source,
+            result: ret,
             beforeResult: args1[0] ? args1[0] : args1[1],
             type: func.type,
             start,
             end,
-            test: 'for callback',
+            test: 'for return',
           });
-          this.callback.apply(this, args);
-        }.bind(this),
-      });
+        }
+        return ret;
+      };
+    }
 
-
-      let start = Date.now();
-      // 针对3: 拿到return的结果，即可拿到source。
-      const ret = func.apply(almostThis, args1); // run 这个 loader。args 是代码字符串。
-      let end = Date.now();
-      if (ret && typeof ret === 'string') {
-        // const start = Date.now()
-        let { resourcePath } = this;
-        // const end = Date.now();
-        collectCode({
-          id: resourcePath,
-          name: `${loaderName}【${func.type}】`,
-          result: ret,
-          beforeResult: args1[0] ? args1[0] : args1[1],
-          type: func.type,
-          start,
-          end,
-          test: 'for return',
-        });
-      }
-      return ret;
-    };
-    // if (/url-loader/.test(path)) {
-    //   debugger
-    // }
-    if (loader.normal) {
+  
+    if (loader.normal) { // normal loader
       if (!loader.normalRaw) {
         loader.normalRaw = loader.normal;
         loader.normalRaw.type = 'Normal';
@@ -123,7 +131,7 @@ function pitch(p) { // p绝对路径
       loader.normal = wrapFunc(loader.normalRaw);
     }
 
-    if (loader.default) {
+    if (loader.default) {// 也是 normal loader(和上面一样)
       if (!loader.defaultRaw) {
         loader.defaultRaw = loader.default;
         loader.defaultRaw.type = 'Normal';
@@ -131,7 +139,7 @@ function pitch(p) { // p绝对路径
       loader.default = wrapFunc(loader.defaultRaw);
     }
 
-    if (loader.pitch) {
+    if (loader.pitch) { // pitch loader (pitch loader一般是用来处理import路径的，这里也收集它处理后的字符串)
       if (!loader.pitchRaw) {
         loader.pitchRaw = loader.pitch;
         loader.pitchRaw.type = 'Pitch';
@@ -176,7 +184,7 @@ function hijackLoaders(loaderPaths, proxyLoader) {
       if (loaderPaths.includes(pathString) && !/my\-inspect\-webpack\-plugin/.test(pathString)) {
         return proxyLoader(target, pathString); // 返回代理loader
       }
-      
+
       // 否则，什么都不做，requrie啥，就返回啥！
       return target
     };
